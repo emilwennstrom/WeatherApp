5ad84a98b080c2b1e22e8bdead6a7e6485c20090
Emil Wennström <emilwennstrom@gmail.com>
2023-11-28 20:04:27 +0100

    Update AppDatabase.kt

--- app/src/main/java/algot/emil/persistence/AppDatabase.kt
+++ app/src/main/java/algot/emil/persistence/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Weather::class, WeatherHourly::class], exportSchema = false, version = 2)
+@Database(entities = [Weather::class, WeatherHourly::class], exportSchema = false, version = 3)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun weatherDao(): WeatherDao
     abstract fun weatherHourlyDao(): WeatherHourlyDao
